56c7c1fabadd903a30c8d446dceb31f1eb7b28b4
MAPSLabCU <mapsgroup.floor13@gmail.com>
2025-09-26 02:06:09 +0700

    Update abstract.js

--- js/abstract.js
+++ js/abstract.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("abstractForm");
 const formStatus = document.getElementById("formStatus");
 
-const openDate = new Date("2025-10-16T00:00:00+07:00");
+const openDate = new Date("2025-09-16T00:00:00+07:00");
 const closeDate = new Date("2025-11-28T23:59:00+07:00");
 const now = new Date();
 
@@ -21,3 +21,4 @@ if (now < openDate) {
   formStatus.classList.add("alert-success");
   formStatus.innerHTML = `<b>The abstract submission is now open.</b><br>You can submit your abstract until <u><b>28 November 2025, 11:59 PM</b></u>.`;
 }
+
